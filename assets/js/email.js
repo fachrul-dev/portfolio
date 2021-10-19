996145d3c74099244f27b9da128f59ad48df69b7
Fachrul Hidayat <fachrul.dev99@gmail.com>
2021-10-19 17:31:02 +0700

    update terbaru

---
+++ assets/js/email.js
@@ -0,0 +1,17 @@
+// listen for a submit
+
+function sendEmail() {
+  let name = document.querySelector('#name').value;
+  let email = document.querySelector('#email').value;
+  let message = document.querySelector('#message').value;
+  let subject = document.querySelector('#subject').value;
+  Email.send({
+    Host: 'smtp.gmail.com',
+    Username: 'fachrul.hidayat15@gmail.com',
+    Password: 'puckvbxoqililxmp',
+    To: 'fachrul.hidayat15@gmail.com',
+    From: `${email}`,
+    Subject: `${subject}`,
+    Body: `Name: ${name} <br/> Email: ${email} <br/> ${message}`,
+  }).then((message) => alert('mail sent successfully'));
+}
